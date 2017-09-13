2bfcccf6092a8abed4fbf9b3cec75dfc96c0b7b0
Jhonnatan-Garcia <jhonnatan.razo.sc@itszapopan.edu.mx>
2017-09-13 14:12:48 -0500

    Creacion Del Metodo Raiz Con Parametros, Sin Retorno

--- Funcion.java
+++ Funcion.java
@@ -1,6 +1,8 @@
 public abstract class Funcion{
 
-
+    //Metodo Abstracto Que No Define Aun Los Metodos De La Clase
     public abstract double evaluacion(double x);
+
+
 
 }
--- Regla_Falsa.java
+++ Regla_Falsa.java
@@ -1,5 +1,11 @@
 public class Regla_Falsa{
 
+    //Creamos El Metodo Para Calcular La Raiz De La Ecuacion Oh Funcion
 
+    public double raiz(Funcion F, double X0, double X1, int iteraciones, double error){
+
+        
+
+    }
 
 }
