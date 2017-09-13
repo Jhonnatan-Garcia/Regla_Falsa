79db4ebe7f6d5819f44f2e88eff6f34f99d42be4
Jhonnatan-Garcia <jhonnatan.razo.sc@itszapopan.edu.mx>
2017-09-13 15:18:39 -0500

    Agregamos Comentarios De Parametros Ingresados

--- Main.java
+++ Main.java
@@ -6,12 +6,23 @@ public class Main{
       Funcion funcion = new Funcion() {
           @Override
           public double evaluacion(double x) {
+              //Funcion A Evaluar En Este Caso: (Sin X) * X
               return Math.sin(x)*x;
           }
       };
 
         Regla_Falsa regla_falsa = new Regla_Falsa();
 
+        //Los Parametros Que Ingresamos Son Los Siguientes:
+        /*
+
+        funcion = (Sin X) * X previamente definida
+        1 = X0
+        4 = X1
+        100 = Numero De iteraciones
+        1e-6 = Margen De Error
+        
+        */
         double Raiz = regla_falsa.raiz(funcion, 1, 4, 100, 1e-6);
         System.out.println("Raiz: " + Raiz);
 }
