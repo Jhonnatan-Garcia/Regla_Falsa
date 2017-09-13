e1407b45524611794ed87bfc685e33c4a547290a
Jhonnatan-Garcia <jhonnatan.razo.sc@itszapopan.edu.mx>
2017-09-13 15:10:08 -0500

    Correcion De Error Override

--- Main.java
+++ Main.java
@@ -3,20 +3,16 @@ public class Main{
 
     public static void main(String[] args){
 
-        Funcion function = new Funcion(){
-
-            @Override
-            public double Funcion(double x){
-
-                return Math.sin(x)*x;
-
-            }
-
-        };
+      Funcion funcion = new Funcion() {
+          @Override
+          public double evaluacion(double x) {
+              return Math.sin(x)*x;
+          }
+      };
 
         Regla_Falsa regla_falsa = new Regla_Falsa();
 
-        double Raiz = regla_falsa.raiz(function, 1, 4, 100, 1e-6);
+        double Raiz = regla_falsa.raiz(funcion, 1, 4, 100, 1e-6);
         System.out.println("Raiz: " + Raiz);
 }
 
