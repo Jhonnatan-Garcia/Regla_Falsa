78de91b3d4c1d3b14f2e354e39dc4ab8af1a1abd
Jhonnatan-Garcia <jhonnatan.razo.sc@itszapopan.edu.mx>
2017-09-13 18:30:47 -0500

    Agregacion De Metodo Para Definir 4 Decimales De Impresion

--- Main.java
+++ Main.java
@@ -24,8 +24,10 @@ public class Main{
         1e-6 = Error Permitido
 
         */
-        double Raiz = regla_falsa.raiz(funcion, 1, 4, 100, 1e-6);
-        System.out.println("Raiz: " + Raiz);
+        double Raiz = regla_falsa.raiz(funcion, 1, 2, 100, 1e-6);
+        String.format("%.4f", Raiz);
+        //System.out.println("Raiz: " + Raiz);
+        System.out.println("Raiz: " + String.format("%.4f", Raiz));
 }
 
 }
