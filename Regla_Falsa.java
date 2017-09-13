a7b9f979c444db0939f71250cbc019b19edb18e1
Jhonnatan-Garcia <jhonnatan.razo.sc@itszapopan.edu.mx>
2017-09-13 14:43:20 -0500

    Terminacion Del Metdo De Regla Falsa

--- Regla_Falsa.java
+++ Regla_Falsa.java
@@ -19,7 +19,27 @@ public class Regla_Falsa{
             //Aproximacion A La Raiz De X
             X = (X0 * F.evaluacion(X1) - X1 * F.evaluacion(X0)) / (F.evaluacion(X1) - F.evaluacion(X0));
 
+            if(F.evaluacion(X0) * F.evaluacion(X) < 0){
+
+              X1 = X;
+
+            }else{
+
+              X0 = X;
+
+            }
+
+            Ciclos++;
+
         }
+
+        if(Ciclos < iteraciones){
+
+            Retorno = X;
+
+        }
+
+        return Retorno;
 
     }
 
