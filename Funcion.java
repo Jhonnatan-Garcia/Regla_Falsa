b2584965d9583f811e21a69df37a4bb0341d5f1a
Jhonnatan-Garcia <jhonnatan.razo.sc@itszapopan.edu.mx>
2017-09-13 15:01:23 -0500

    Implementacion De Los Metodos

--- Funcion.java
+++ Funcion.java
@@ -3,6 +3,4 @@ public abstract class Funcion{
     //Metodo Abstracto Que No Define Aun Los Metodos De La Clase
     public abstract double evaluacion(double x);
 
-
-
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
+import java.util.*;
 public class Main{
 
     public static void main(String[] args){
 
+        Funcion function = new Funcion(){
 
+            @Override
+            public double Funcion(double x){
 
-    }
+                return Math.sin(x)*x;
+
+            }
+
+        };
+
+        Regla_Falsa regla_falsa = new Regla_Falsa();
+
+        double Raiz = regla_falsa.raiz(function, 1, 4, 100, 1e-6);
+        System.out.println("Raiz: " + Raiz);
+}
 
 }
